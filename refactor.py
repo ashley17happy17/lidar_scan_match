016720bbd31f0d0cb5038e22978c332af0e3c92e
ashley <ashley.yt.chiu@foxconn.com>
2026-08-13 15:25:16 +0800

    refactor: remove unused refactor.py script to clean up the repository

--- refactor.py
+++
@@ -1,9 +0,0 @@
-import re
-import os
-
-with open('src/lidar_slam_node.cpp', 'r') as f:
-    content = f.read()
-
-# We will just do a simple rename and split if possible.
-# Actually, a full C++ parser is hard. 
-# Let's just rename the class and extract main.cpp first to see if that's what the user wants.
